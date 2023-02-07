839f6794f30ed7b77dae68ebe4990440fee9b440
venkatesh-na <naikvenkatesh723@gmail.com>
2023-02-07 21:57:32 +0530

    adsad

--- js/cart.js
+++ js/cart.js
@@ -16,8 +16,28 @@ let paramaters = (new URL(url)).searchParams;
 //fetch id from url
 var id = paramaters.get("id");
 
-var filteredData = {...data.filter(d=>d.id == id)[0],count : 0}
+function isProductExists(id){
+    console.log(carts.findIndex(c=>c.id == id)) 
+    if(carts.findIndex(c=>c.id == id) != -1){
+        return true;
+    } else {
+        return false;
+}
+}
+
+var filteredData = {...data.filter(d=>d.id == id)[0],count : 1}
+if(!isProductExists(id)){
 carts.push(filteredData);
+} else {
+    carts = carts.map(c=>{
+        const {count } = c
+        if(c.id == id){
+            return {...c,count : count + 1}
+        } else return c
+    })
+}
+
+
 
 //add cart data to localstorage
 localStorage.setItem("cart",JSON.stringify(carts))
@@ -38,11 +58,45 @@ items.innerHTML = carts.map(e=>{
         <div class="btn dec">-</div>
     </div>
     <div class="prices">
-        <div class="amount">${price}</div>
-        <div class="save"><u>Save for later</u></div>
-        <div class="remove"><u>Remove</u></div>
+        <div class="amount">&#x20B9;${price}</div>
+        <div class="remove"><i class="fa fa-trash-o"></i></div>
     </div>
 </div>
 `
 }).join("")
+
+const AllRemoveBtn = document.querySelectorAll(".remove")
+AllRemoveBtn.forEach(btn=>{
+    btn.addEventListener("click",(e)=>{
+        const id = e.target.parentElement.parentElement.parentElement.id;
+        console.log(id)
+        carts = carts.filter(c=>c.id != id)
+        localStorage.setItem("cart",JSON.stringify(carts))
+
+        items.innerHTML = carts.map(e=>{
+            const {  id, src, name, rating, price, description, count } = e;
+            return `<div id = "${id}" class="Cart-Items">
+            <div class="image-box">
+                <img src=${src} style={{ height=”120px” }} />
+            </div>
+            <div class="about">
+                <h1 class="title">${name}</h1>
+                <p>Rating : ${rating}</p>
+            </div>
+            <div class="counter">
+                <div class="btn inc">+</div>
+                <div class="count">${count}</div>
+                <div class="btn dec">-</div>
+            </div>
+            <div class="prices">
+                <div class="amount">&#x20B9;${price}</div>
+                <div class="remove"><i class="fa fa-trash-o"></i></div>
+            </div>
+        </div>
+        `
+        }).join("")
+        
+    })
+})
+
 
--- js/script.js
+++ js/script.js
@@ -1,8 +1,78 @@
+let tempData = [...data]
+
 let preveiwContainer = document.querySelector('.products-preview');
 
 let productContainer = document.querySelector('.products-container');
 
-productContainer.innerHTML=data.map((product)=>{
+const filteredButton = document.querySelectorAll(".filter button");
+
+filteredButton.forEach((btn)=>{
+  btn.addEventListener("click",(event)=>{
+    const category = event.target.id;
+
+    if(category == "ALL") tempData = [...data]
+    else tempData = data.filter(e=>e.category == category)
+
+    productContainer.innerHTML=tempData.map((product)=>{
+      const { id, src, name, rating, price, description } = product 
+      return `<div class="product" data-name="${id}">
+               <img src="${src}" alt="">
+               <h3>${name}</h3>
+               <div class="price">₹${price}/-</div>
+            </div>`
+      })
+
+      preveiwContainer.innerHTML=tempData.map((product)=>{
+        const { id, src, name, rating, price, description} = product
+        return `<div class="preview" data-target="${id}">
+        <i class="fas fa-times"></i>
+        <img src="${src}" alt="">
+        <h3>${name}</h3>
+        <div class="stars">
+           <i class="fas fa-star"></i>
+           <i class="fas fa-star"></i>
+           <i class="fas fa-star"></i>
+           <i class="fas fa-star"></i>
+           <i class="fas fa-star-half-alt"></i>
+           <span>( 250 )</span>
+        </div>
+        <p> ${description}</p>
+        <div class="price">₹${price}/-</div>
+        <div class="buttons">
+           <a href="#" class="buy">buy now</a>
+           <a href="Cart.html?id=${id}" class="cart">add to cart</a>
+        </div>
+     </div>`
+      })
+    
+    let previewBox = preveiwContainer.querySelectorAll('.preview');
+    document.querySelectorAll('.products-container .product').forEach(product =>{
+      product.onclick = () =>{
+        preveiwContainer.style.display = 'flex';
+        let name = product.getAttribute('data-name');
+    
+        previewBox.forEach(preview =>{
+          let target = preview.getAttribute('data-target');
+          if(name == target){
+            preview.classList.add('active');
+          }
+        });
+    
+      };
+    });
+    
+    previewBox.forEach(close =>{
+      close.querySelector('.fa-times').onclick = () =>{
+        close.classList.remove('active');
+        preveiwContainer.style.display = 'none';
+      };
+    });
+
+  })
+})
+
+
+productContainer.innerHTML=tempData.map((product)=>{
   const { id, src, name, rating, price, description } = product 
   return `<div class="product" data-name="${id}">
            <img src="${src}" alt="">
@@ -11,7 +81,7 @@ productContainer.innerHTML=data.map((product)=>{
         </div>`
   })
 
-  preveiwContainer.innerHTML=data.map((product)=>{
+  preveiwContainer.innerHTML=tempData.map((product)=>{
     const { id, src, name, rating, price, description} = product
     return `<div class="preview" data-target="${id}">
     <i class="fas fa-times"></i>
@@ -33,21 +103,20 @@ productContainer.innerHTML=data.map((product)=>{
     </div>
  </div>`
   })
+
 let previewBox = preveiwContainer.querySelectorAll('.preview');
 document.querySelectorAll('.products-container .product').forEach(product =>{
   product.onclick = () =>{
     preveiwContainer.style.display = 'flex';
     let name = product.getAttribute('data-name');
-    console.log(name);
-    console.log(previewBox);
+
     previewBox.forEach(preview =>{
       let target = preview.getAttribute('data-target');
-      console.log(target);
       if(name == target){
-        console.log(name,target)
         preview.classList.add('active');
       }
     });
+
   };
 });
 
